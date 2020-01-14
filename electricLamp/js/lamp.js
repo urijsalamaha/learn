b65a5010ec9e1eea0e7c2f1c048f63c2bac19075
urijsalamaha <59803893+urijsalamaha@users.noreply.github.com>
2020-01-14 23:32:28 +0200

    Update lamp.js

--- electricLamp/js/lamp.js
+++ electricLamp/js/lamp.js
@@ -10,14 +10,14 @@ imgBreaker.addEventListener('click', function () {
     var x = ++count;
     console.log(x);
     if (x % 2 === 0) {
-        imgLamp.setAttribute('src', '../images/lampOff.png');
-        imgBreaker.setAttribute('src', '../images/circuit breakerOff.png');
-        sound.innerHTML = "<audio src = '../sound/breakerSoundOff.wav' autoplay = 'autoplay'></audio>" 
+        imgLamp.setAttribute('src', '/images/lampOff.png');
+        imgBreaker.setAttribute('src', '/images/circuit breakerOff.png');
+        sound.innerHTML = "<audio src = '/sound/breakerSoundOff.wav' autoplay = 'autoplay'></audio>" 
         count = 0;
     }
     else {
-        imgLamp.setAttribute('src', '../images/lampOn.png');
-        imgBreaker.setAttribute('src', '../images/circuit breakerOn.png');
-        sound.innerHTML = "<audio src = '../sound/breakerSoundOn.mp3' autoplay = 'autoplay'></audio>"
+        imgLamp.setAttribute('src', '/images/lampOn.png');
+        imgBreaker.setAttribute('src', '/images/circuit breakerOn.png');
+        sound.innerHTML = "<audio src = '/sound/breakerSoundOn.mp3' autoplay = 'autoplay'></audio>"
     }
 })
