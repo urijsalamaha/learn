1621650bcf0ea9ff733b767539f7edf4e53c6777
Юрий Саламаха <@Lenovo-PC>
2020-01-14 21:42:51 +0200

    electricLamp

---
+++ electricLamp/js/lamp.js
@@ -0,0 +1,23 @@
+var imgBreaker = document.querySelector('#breaker');
+var imgLamp = document.querySelector('#lamp');
+var sound = document.querySelector('.sound');
+var count = 0; //* Counter of events. If lamp On, takes value - 1 and if lamp Off, value - 2.
+
+/**
+ * Function "Lamp On/Off"
+ */
+imgBreaker.addEventListener('click', function () {
+    var x = ++count;
+    console.log(x);
+    if (x % 2 === 0) {
+        imgLamp.setAttribute('src', '../images/lampOff.png');
+        imgBreaker.setAttribute('src', '../images/circuit breakerOff.png');
+        sound.innerHTML = "<audio src = '../sound/breakerSoundOff.wav' autoplay = 'autoplay'></audio>" 
+        count = 0;
+    }
+    else {
+        imgLamp.setAttribute('src', '../images/lampOn.png');
+        imgBreaker.setAttribute('src', '../images/circuit breakerOn.png');
+        sound.innerHTML = "<audio src = '../sound/breakerSoundOn.mp3' autoplay = 'autoplay'></audio>"
+    }
+})
